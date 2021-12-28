f0a89de26a7df45ffd91dcb9117e8f4fc732edaa
Brendan <Brendan.m.Fischer@gmail.com>
2021-12-27 20:06:50 -0500

    got roadrunner code to build

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetection.java
@@ -51,7 +51,7 @@ import java.util.List;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
+@TeleOp(name = "Concept: TensorFlow Object Detection 2", group = "Concept")
 //@Disabled
 public class
 ConceptTensorFlowObjectDetection extends LinearOpMode {
